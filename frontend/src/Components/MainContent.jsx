d75115343314d7d48410cd2ad10e2ab819a43868
Kanak Yadav <yadavkanak109@gmail.com>
2025-11-02 17:10:33 +0530

    complete Gernate Meam Project

--- frontend/src/Components/MainContent.jsx
+++ frontend/src/Components/MainContent.jsx
@@ -2,32 +2,68 @@ import React, { useState } from 'react'
 const MainContent = () => {
 
   const [meam,setmeam] = useState({
-    Top_text: "Top text",
-    Bottom_text:"Bottom text",
+    topText: "top text",
+    bottomText:"Bottom text",
     image : "/src/assets/AI.png"
   })
+  const [allmeme ,setallmeme]  = React.useState([])
+
+  React.useEffect(()=>{
+    fetch("https://api.imgflip.com/get_memes")
+    .then(res=>res.json())
+    .then(data =>setallmeme(data.data.memes) )
+  },[])
+
+  function eventHandeler(event){
+   const {value,name} = event.currentTarget;
+   setmeam(preMeam => ({
+    ...preMeam,
+      [name] :value
+
+
+   }))
+  
+  }
+   function getMemeImage() {
+     event.preventDefault();
+    let randomImage = Math.floor(Math.random()*allmeme.length)
+
+   let newMeamUrl = allmeme[randomImage].url
+
+   setmeam(preMeam=>({
+
+    ...preMeam,
+
+    image : newMeamUrl
+
+   }))
+
+   }
+
+  
   return (
     <>
+    
     <form action="" className='form' >
        <div className='form-inputs'>
        <div className='input-1'> 
        
         <label htmlFor="upper-text"><b>Top Text</b></label>
-        <input type="text" id="upper-text" placeholder='Enter Upper Text'/>
+        <input name="topText" type="text" value={meam.topText} id="upper-text" onChange={eventHandeler} placeholder='Enter Upper Text'/>
         </div>
         <div className='input-2'>
         <label htmlFor="bottom-text"><b>Bottom text</b></label>
-        <input type="text" id="bottom-text" placeholder='Enter Bottom Text'/>
+        <input name="bottomText" value={meam.bottomText} onChange={eventHandeler} type="text" id="bottom-text" placeholder='Enter Bottom Text'/>
         </div>
          </div>
     
-    <button>Get a new meme image 😶‍🌫️</button>
+    <button type="button" onClick={getMemeImage}>Get a new meme image 😶‍🌫️</button>
     </form>
     
     <div className='ai-image'>
-      <img  src={meam.image} alt="ai-image" />
-      <span className='top'>{meam.Top_text}</span>
-      <span className='bottom'>{meam.Bottom_text}</span>
+      <img  src={meam.image} alt="imageUrl" />
+      <span className='top'>{meam.topText}</span>
+      <span className='bottom'>{meam.bottomText}</span>
       </div>
     </>
 
